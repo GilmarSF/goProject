3f84119d81e2a718831c0eb169a51e0a844a3518
Joseph <joseluis.bsa@gmail.com>
2017-04-25 09:06:24 -0300

    Update main.go

--- conn-banco/src/main/main.go
+++ conn-banco/src/main/main.go
@@ -8,12 +8,11 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
-var dbDados string
-var slcDados = make([]string, 0)
+var dbDados string // string criada para receber os valores do objeto
+var slcDados = make([]string, 0) // slice criado para guardar as strings que o dbDados recebe
 
 func main(){
 	connectionDB()   // inicia conexão com o azure
@@ -23,7 +22,7 @@ func connectionDB() {
 
 	// Banco no azure do Zé
 	db, err := sql.Open("mssql", "server=pwbt.database.windows.net;user id=admin-jose;password=123abc!@#;database=PWBT;port=1433")
-	if err != nil {
+	if err != nil { // se o retorno for 'nil' quer dizer que deu tudo certo
 		log.Println("Open Failed: ", err.Error())
 	}
 
@@ -39,12 +38,12 @@ func connectionDB() {
 	
 	// rows.Next usado para varrer o objeto 'rows' e pegar os valores retornados da Query
 	for rows.Next() {
-	
+		// percorre o objeto 'rows' salvando o que encontra na string 'dbDados'
 		if err := rows.Scan(&dbDados); err != nil { 
 			log.Fatal(err)
 		}
 		fmt.Printf("Categoria: %s \n", dbDados)
-		slcDados = append(slcDados, dbDados) //salva o retorno neste slice "slice=array flexivel"
+		slcDados = append(slcDados, dbDados) //salva o retorno neste slice "slice=array flexivel" append=adicionar no final
 	}
 
 	fmt.Println(slcDados)  // exibe todo conteudo do slice
