1087da16436796cd2834479127825c5ad5d6d88d
Joseph <joseluis.bsa@gmail.com>
2017-04-26 22:14:19 -0300

    Update main.go

--- simple-site/src/main/main.go
+++ simple-site/src/main/main.go
@@ -6,33 +6,33 @@ import (
   "html/template"
 )
 
+// struct que sera usada para exibir as 'strings' na pagina
 type content struct{
 	Nome string
 	Idade string
 }
  
 func main() {
-  http.HandleFunc("/", index)
-  http.HandleFunc("/greet", greeter)
+  http.HandleFunc("/", index) // chama a pagina principal
+  http.HandleFunc("/greet", greeter) // chama a pagina com a mensagem
   log.Println("Listening...")
-  err := http.ListenAndServe(":80", nil)
+  err := http.ListenAndServe(":80", nil) // servidor http na porta 80 padrão web
   if err != nil {
     log.Fatal("ListenAndServe: ", err)
    }
 }
  
 func index(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("public/index.html")
-  t.Execute(w, nil)
+  t, _ := template.ParseFiles("public/index.html") // salva o html em 't'
+  t.Execute(w, nil) // exibe a pagina index.html
 }
  
 func greeter(w http.ResponseWriter, r *http.Request) {
-  dados := content{}
-  
-  dados.Nome = r.FormValue("nome")
+  dados := content{} // dados do tipo struct
+  dados.Nome = r.FormValue("nome") // salva o nome na struct dados
   dados.Idade = r.FormValue("idade")
-  t, _ := template.ParseFiles("public/greeter.html")
-  err := t.Execute(w, dados)
+  t, _ := template.ParseFiles("public/greeter.html") // salva o html em 't'
+  err := t.Execute(w, dados) // para exibir a pagina greeter.html
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }
